ea0b5dfcb8902da05017bc97e9c0884ceec2ee65
Aleksandra <aleszczupak@gmail.com>
2024-10-15 14:00:59 +0200

    Update 2.10.py

--- Zadania 2/2.10.py
+++ Zadania 2/2.10.py
@@ -1,7 +1,10 @@
 with open('infile.txt', 'r') as file:
     line = file.read()
 
+#line = 'Pierwsza linijka\tto jest\na teraz druga!\ntrzecia\nGvR\tkoniec.'
+
 # domyślnie każdy biały znak jest separatorem split()
 num_of_words = len(line.split())
 assert num_of_words == 16
+#assert num_of_words == 10
 print(num_of_words)
