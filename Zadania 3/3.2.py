365f5dec997b8b59f97a6da30808e12fbcae6efe
Aleksandra <aleszczupak@gmail.com>
2024-10-23 07:41:16 +0200

    Create 3.2.py

---
+++ Zadania 3/3.2.py
@@ -0,0 +1,28 @@
+# 1
+L = [3, 4, 5]
+L = L.sort()
+# nie jest potrzebna wartość przypisania
+
+# 2
+# x, y = 1, 2, 3
+# za mało zmiennych, za dużo liczb do przypisania
+
+# 3
+X = 1, 2, 3
+print(X) # (1, 2, 3)
+# X[1] = 4
+# krotka jest niezmienna - nie można wykonać operacji przypisania
+
+# 4
+X = [1, 2, 3]
+# X[3] = 4
+# nieistniejący indeks
+
+# 5
+X = 'abc'
+# X.append("d")
+# str nie ma atrybutu append()
+
+# 6
+# L = list(map(pow, range(8)))
+# brak argumentu w funkcji pow(liczba, wykładnik)
