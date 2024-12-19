4caf6f10f0704105713bb2bb8a35c5e5135595d6
Aleksandra <aleszczupak@gmail.com>
2024-12-19 14:24:13 +0100

    Create double_node.py

---
+++ Zadania 11/double_node.py
@@ -0,0 +1,16 @@
+class Node:
+    '''Klasa reprezentująca węzeł listy dwukierunkowej.'''
+
+    def __init__(self, data=None, next=None, prev=None):
+        self.data = data
+        self.next = next
+        self.prev = prev
+
+    def __str__(self):
+        return str(self.data)
+
+    def __eq__(self, other):
+        return self.data == other.data
+
+    def __ne__(self, other):
+        return not self == other
